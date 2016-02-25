512396ca811d613d86a8bf81e4a4e70ac6334d11
pcorentin <c.pescheloche@gmail.com>
2016-02-25 00:56:37 -0800

    Why clearRect hates me ! First animation

--- grid.js
+++ grid.js
@@ -14,8 +14,12 @@ var canvas = $("#game");
 var context = $("#game").get(0).getContext("2d");
 context.canvas.height = ch;
 context.canvas.width = cw;
+context.globalCompositeOperation = 'destination-over';
+
+
 
 function drawBoard(){
+    context.beginPath();
     for (var x = 0; x <= bw; x += 40) {
         context.moveTo(0.5 + x + p, p);
         context.lineTo(0.5 + x + p, bh + p);
@@ -26,18 +30,28 @@ function drawBoard(){
         context.moveTo(p, 0.5 + x + p);
         context.lineTo(bw + p, 0.5 + x + p);
     }
-
+context.closePath();
     context.strokeStyle = "black";
     context.stroke();
 }
 
 function drawPlayer(player) {
+
 canvas.drawArc({
   fillStyle: player.color,
   x: player.xPos, y: player.yPos,
   radius: player.radius
 });
+
 }
+
+function clearGrid() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+}
+
+
+
+
 
 
 
